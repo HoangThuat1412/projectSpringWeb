62af33490e67329a2f6c70cb40cbeedf0bcd9548
HoangThuat1412 <thuathvhd@gmail.com>
2025-03-20 14:32:24 +0700

    tạo dự án và ghép giao diện admin

---
+++ project/src/main/java/com/projectvn/controllers/HomeController.java
@@ -0,0 +1,14 @@
+package com.projectvn.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class HomeController {
+	@RequestMapping("")
+	public String home() {
+		return "index";
+	}
+	
+}
